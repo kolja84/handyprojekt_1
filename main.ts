6c058256872724f57f3a6fcec0fc3200242abaa5
kolja84 <131755123+kolja84@users.noreply.github.com>
2023-05-17 11:15:07 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+enum RadioMessage {
+    message1 = 49434,
+    Hallo = 324,
+    Tschüss = 7978,
+    Ja = 47166,
+    Nein = 13013
+}
 /**
  * Homescreen
  */
@@ -8,12 +15,16 @@ radio.onReceivedString(function (receivedString) {
     let recivedstring = 0
     basic.showString("" + (recivedstring))
 })
+let Programm_Nachrichten_msg_2 = 0
 let Programm_Musik_Song = 0
-let Programm_Nachrichten_Kanal_2 = 0
+let Programm_Nachrichten_msg_1 = 0
 let Programm_Nachrichten_Kanal_1 = 0
 let Programm_Zufallszahl_MaximaleZahl = 0
 let Programm_MusikPlay_Pause = 0
+let Programm_Nachrichten_Kanal_2 = 0
 let Standby = 0
+let Programm_Galerie_2 = 0
+let Programm_Galerie_1 = 0
 let Programm_Zufallszahlengenartor_1 = 0
 let Programm_Musik = 0
 let Programm_Taschenrechner = 0
@@ -120,12 +131,38 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
+    if (Homescreen_1 == 5) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . # . #
+            # . . . #
+            # # # # #
+            `)
+        if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+            Homescreen_1 = 0
+            Programm_Galerie_1 = 1
+            Programm_Galerie_2 = 1
+        }
+        if (input.buttonIsPressed(Button.A)) {
+            Homescreen_1 += -1
+        }
+        if (input.buttonIsPressed(Button.B)) {
+            Homescreen_1 += 1
+        }
+    }
+})
+/**
+ * Programm:e
+ */
+basic.forever(function () {
     if (input.buttonIsPressed(Button.AB) && Homescreen_1 == 0) {
-        Homescreen_1 = 1
         music.playTone(147, music.beat(BeatFraction.Whole))
         Messenger_Variable_1 = 0
         Programm_Musik = 0
         Programm_Taschenrechner = 0
+        Programm_Galerie_1 = 0
+        Homescreen_1 = 1
     }
     if (Homescreen_1 > 0) {
         Messenger_Variable_1 = 0
@@ -147,6 +184,11 @@ basic.forever(function () {
     }
     if (Standby == 1 && input.buttonIsPressed(Button.AB)) {
         Homescreen_1 = 1
+    }
+})
+basic.forever(function () {
+    if (Messenger_Variable_1 == 1) {
+        radio.setGroup(Programm_Nachrichten_Kanal_2)
     }
 })
 basic.forever(function () {
@@ -183,24 +225,6 @@ basic.forever(function () {
         Programm_Zufallszahl_MaximaleZahl = pins.analogReadPin(AnalogPin.P2)
     }
 })
-/**
- * Programm:e
- */
-basic.forever(function () {
-    if (Messenger_Variable_1 == 1) {
-        radio.setGroup(Programm_Nachrichten_Kanal_1)
-    }
-})
-basic.forever(function () {
-    if (Messenger_Variable_1 == 1) {
-        Programm_Nachrichten_Kanal_1 = Math.map(pins.analogReadPin(AnalogPin.P2), 0, 1023, 0, 4)
-        basic.pause(500)
-        Programm_Nachrichten_Kanal_2 = Programm_Nachrichten_Kanal_1
-        if (Programm_Nachrichten_Kanal_1 != Programm_Nachrichten_Kanal_2) {
-            basic.showNumber(Programm_Nachrichten_Kanal_1)
-        }
-    }
-})
 basic.forever(function () {
     if (Programm_Taschenrechner == 1) {
         basic.showLeds(`
@@ -210,6 +234,94 @@ basic.forever(function () {
             . . . . .
             . . . . .
             `)
+    }
+})
+basic.forever(function () {
+    if (Programm_Galerie_1 == 1) {
+        if (input.buttonIsPressed(Button.A)) {
+            Programm_Galerie_2 += -1
+        }
+        if (input.buttonIsPressed(Button.A)) {
+            Programm_Galerie_2 += 1
+        }
+    }
+})
+basic.forever(function () {
+    if (Messenger_Variable_1 == 1 && !(input.buttonIsPressed(Button.A))) {
+        Programm_Nachrichten_Kanal_1 = Math.map(pins.analogReadPin(AnalogPin.P2), 0, 1023, 0, 4)
+        Programm_Nachrichten_Kanal_2 = Math.round(Programm_Nachrichten_Kanal_1)
+        basic.showNumber(Programm_Nachrichten_Kanal_2)
+    }
+    if (!(input.buttonIsPressed(Button.A))) {
+        Programm_Nachrichten_msg_1 = 0
+        radio.setGroup(Programm_Nachrichten_Kanal_2)
+        basic.pause(300)
+    }
+})
+basic.forever(function () {
+    if (Programm_Galerie_1 == 1 && Programm_Galerie_2 == 1) {
+        basic.showIcon(IconNames.Heart)
+    }
+    if (Programm_Galerie_1 == 1 && Programm_Galerie_2 == 2) {
+        basic.showIcon(IconNames.Happy)
+    }
+    if (Programm_Galerie_1 == 1 && Programm_Galerie_2 == 3) {
+        basic.showIcon(IconNames.Sad)
+    }
+    if (Programm_Galerie_1 == 1 && Programm_Galerie_2 == 4) {
+        basic.showIcon(IconNames.Confused)
+    }
+    if (Programm_Galerie_1 == 1 && Programm_Galerie_2 == 5) {
+        basic.showIcon(IconNames.Angry)
+    }
+    if (Programm_Galerie_1 == 1 && Programm_Galerie_2 == 6) {
+        basic.showIcon(IconNames.Asleep)
+    }
+    if (Programm_Galerie_1 == 1 && Programm_Galerie_2 == 7) {
+        basic.showIcon(IconNames.Surprised)
+    }
+    if (Programm_Galerie_1 == 1 && Programm_Galerie_2 == 8) {
+        basic.showIcon(IconNames.Silly)
+    }
+    if (Programm_Galerie_1 == 1 && Programm_Galerie_2 == 8) {
+        basic.showIcon(IconNames.Fabulous)
+    }
+    if (Programm_Galerie_1 == 1 && Programm_Galerie_2 == 9) {
+        basic.showIcon(IconNames.TShirt)
+    }
+    if (Programm_Galerie_1 == 1 && Programm_Galerie_2 == 10) {
+        basic.showIcon(IconNames.Rollerskate)
+    }
+    if (Programm_Galerie_1 == 1 && Programm_Galerie_2 == 11) {
+        basic.showIcon(IconNames.Rollerskate)
+    }
+})
+basic.forever(function () {
+    if (Messenger_Variable_1 == 1) {
+        if (Programm_Nachrichten_msg_1 == 1) {
+            basic.showString("Hallo")
+            if (input.isGesture(Gesture.Shake)) {
+                radio.sendMessage(RadioMessage.Hallo)
+            }
+        }
+        if (Programm_Nachrichten_msg_1 == 2) {
+            basic.showString("tschüss")
+            if (input.isGesture(Gesture.Shake)) {
+                radio.sendMessage(RadioMessage.Tschüss)
+            }
+        }
+        if (Programm_Nachrichten_msg_1 == 3) {
+            basic.showString("Ja")
+            if (input.isGesture(Gesture.Shake)) {
+                radio.sendMessage(RadioMessage.Ja)
+            }
+        }
+        if (Programm_Nachrichten_msg_1 == 4) {
+            basic.showString("Nein")
+            if (input.isGesture(Gesture.Shake)) {
+                radio.sendMessage(RadioMessage.Nein)
+            }
+        }
     }
 })
 basic.forever(function () {
@@ -237,13 +349,9 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    if (Messenger_Variable_1 == 1) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . #
-            `)
+    basic.pause(250)
+    if (input.buttonIsPressed(Button.A)) {
+        Programm_Nachrichten_msg_2 = Math.map(pins.analogReadPin(AnalogPin.P2), 0, 1023, 0, 4)
+        Programm_Nachrichten_msg_1 = Math.round(Programm_Nachrichten_msg_2)
     }
 })
