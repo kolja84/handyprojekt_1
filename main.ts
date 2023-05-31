44066dd8764ab2786fac0bd8ff975677c7e0abf6
kolja84 <131755123+kolja84@users.noreply.github.com>
2023-05-31 10:34:37 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -22,7 +22,6 @@ let Programm_Tascheenrechner_Auswahl_Zahl_2 = 0
 let Programm_Taschenrechner_Lampe = 0
 let Programm_Taschenrechner_Auswahl_Zahl_1 = 0
 let Programm_Musik_Song = 0
-let Programm_Musik_1 = 0
 let Programm_MusikPlay_Pause = 0
 let Standby = 0
 let Programm_Reaktion = 0
@@ -285,10 +284,8 @@ basic.forever(function () {
 })
 basic.forever(function () {
     if (Programm_MusikPlay_Pause == 1) {
-        Programm_Musik_1 = Math.map(pins.analogReadPin(AnalogPin.P2), 0, 1023, 1, 2)
-        Programm_Musik_Song = Math.round(Programm_Musik_1)
+        Programm_Musik_Song = 1
         if (Programm_Musik_Song == 1) {
-            basic.showNumber(1)
             music.playMelody("C5 B A G F E D C ", 270)
             music.playMelody("C D E F G A B C5 ", 248)
             music.playMelody("E B C5 A B G A F ", 248)
@@ -299,12 +296,6 @@ basic.forever(function () {
             music.playMelody("G F G A - F E D ", 307)
             music.playMelody("E D G F B A C5 B ", 248)
             music.playMelody("C5 G B A F A C5 B ", 263)
-        }
-        if (Programm_Musik_Song == 2) {
-            music.playMelody("E G D A B G C5 D ", 118)
-            music.playMelody("F G A B E C5 D C ", 118)
-            music.playMelody("E B C5 A G D F E ", 118)
-            music.playMelody("C G F B D A C C5 ", 120)
         }
     }
 })
@@ -502,11 +493,6 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    if (Programm_Galerie_2 == 12) {
-        basic.showIcon(IconNames.Fabulous)
-    }
-})
-basic.forever(function () {
     if (Programm_Galerie_2 == 6) {
         basic.showIcon(IconNames.Asleep)
     }
@@ -580,11 +566,6 @@ basic.forever(function () {
 basic.forever(function () {
     if (Programm_Galerie_2 == 4) {
         basic.showIcon(IconNames.Confused)
-    }
-})
-basic.forever(function () {
-    if (Programm_Galerie_2 == 11) {
-        basic.showIcon(IconNames.Rollerskate)
     }
 })
 basic.forever(function () {
